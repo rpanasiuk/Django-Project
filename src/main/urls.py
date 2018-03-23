c5c18cb5c455bc8a47261cf90682ab3948035815
Rafal Panasiuk <panasiukraf@gmail.com>
2018-03-23 05:47:11 +0100

    Added basic TemplateView to url (tracking bootstrap template changes)

--- src/main/urls.py
+++ src/main/urls.py
@@ -16,6 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.views.generic import TemplateView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('bootstrap/', TemplateView.as_view(template_name='bootstrap/bootstrap_example.html')),
+    path('django/', TemplateView.as_view(template_name='bootstrap/django.html')),
 ]
