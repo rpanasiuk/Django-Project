d7a2faaed349f65b22a599076a5396b1f2fed62d
Rafal Panasiuk <panasiukraf@gmail.com>
2018-03-26 12:11:47 +0200

    Added project app

--- src/main/settings/base.py
+++ src/main/settings/base.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'products',
 ]
 
 MIDDLEWARE = [
@@ -107,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'EU-WARSAW'
+TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
