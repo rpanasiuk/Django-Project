24a5aec5936a75ace95cd89bccd13e496d20d672
Rafal Panasiuk <panasiukraf@gmail.com>
2018-03-28 16:22:29 +0200

    No changes.

--- src/products/models.py
+++ src/products/models.py
@@ -1,10 +1,13 @@
 from django.db import models
+from django.core.exceptions import FieldDoesNotExist
+from django.http import QueryDict
+
 
 class Product(models.Model):
 	title 			= models.CharField(max_length=120)
 	price 			= models.DecimalField(decimal_places=2, default=99.99, max_digits=12)
 	description 	= models.TextField(max_length=500)
-	category 		= models.ManyToManyField('Category', blank=False)
+	category 		= models.ManyToManyField('Category', blank=True)
 	product_class	= models.ForeignKey(
 										'ProductType', related_name='product_class', 
 										blank=False, null=True, on_delete=models.CASCADE)
